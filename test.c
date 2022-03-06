8c1dac22647398a18d959fe26e2ae9c8c9871265
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-05 23:13:17 -0500

    File execution review

--- test.c
+++ test.c
@@ -1,9 +1,5 @@
 #include <stdio.h> 
 
 int main(){
-    int age;
-    printf("Enter age \n");
-    scanf("%d", &age);
-    printf("age is %d" ,age);
-    return 0;    
+    printf("i don't feel like coding today"); 
 }
