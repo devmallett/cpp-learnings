701ef64ef4f256255fb9e0d1753e790fe96aa542
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-06 23:06:22 -0500

    More numbz

--- numbo/numbs.cpp
+++ numbo/numbs.cpp
@@ -10,7 +10,17 @@ int main()
     // dnum --;
     // cout <<  5.5 + 9;
 
-    cout << pow(10, 5);
+    // cout << pow(10, 5);
+    // cout << sqrt(36);
+
+    // cout << round(1);
+
+    // cout << floor(1.5);
+
+    // cout << fmax(1 ,5);
+
+    cout << fmin(1 ,5);
+
 
     return 0;
 }
