f92c7f4fa9aa33ae70d017147e6092ae3c7fc43c
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-06 22:20:58 -0500

    Variable addition

---
+++ varz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    string characterName = "Rocky";
+    int characterAge;
+    characterAge = 30;
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was 30 years old" << endl;
+    cout << "Hw liked the name Rocky" << endl;
+    cout << "But he loved being 30" << endl;
+
+    
+    return 0;
+}
