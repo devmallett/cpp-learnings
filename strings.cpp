53e7541810ea02353ac71a58cc4a28a610dbdb24
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-07 23:07:42 -0500

    S

--- strings.cpp
+++ strings.cpp
@@ -6,14 +6,14 @@ int main()
 {
 
     string phrase = "this is a phrase";
-    // cout << phrase.length();
-    // phrase[0] = 'd';
-    // cout << phrase.find("phrase", 0);
+    cout << phrase.length();
+    phrase[0] = 'd';
+    cout << phrase.find("phrase", 0);
     
 
-    string phrasesub;
-    phrasesub = phrase.substr(8, 3);
-    cout << phrasesub;
+    // string phrasesub;
+    // phrasesub = phrase.substr(8, 3);
+    // cout << phrasesub;
 
     
     return 0;
