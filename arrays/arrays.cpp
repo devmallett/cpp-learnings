92b773f3401a9c81adb408bc95bc2a3361fac2ec
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-09 22:18:27 -0500

    Array intro

---
+++ arrays/arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    
+
+    int luckyNums[] = {4 ,8 ,15 ,16 ,23 ,42};
+
+    string states[] = {"Ohio" ,"Maine" ,"Texas" ,"Delaware" ,"New York"};
+
+    luckyNums[0] = 19;
+    
+    cout << luckyNums[0] << endl;
+
+    cout << states[0] << endl;
+
+
+    return 0;
+}
