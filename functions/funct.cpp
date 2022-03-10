9ae144e68eefeb1dc4de50e01695c728f916563c
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-09 22:29:40 -0500

    Function stub

--- functions/funct.cpp
+++ functions/funct.cpp
@@ -2,16 +2,23 @@
 
 using namespace std;
 
+//Function Stub (function signature)
 
-void sayHi(string name) {
-    cout << "Hello " << name  << endl;;
-}
+void sayHi(string name ,int age);
+
 
 int main() {
     
     cout << "Top" << endl;
-    sayHi("Dev");
+    sayHi("Dev" ,27);
+    sayHi("Tom" ,45);
+    sayHi("Steve" ,19);
 
     cout << "Bottom" << endl;
     return 0;
+}
+
+
+void sayHi(string name ,int age) {
+    cout << "Hello " << name  << " you are " << age << endl;;
 }
