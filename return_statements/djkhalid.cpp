4fb0655bc51cbf7d9964002b81db7d38966abaa8
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-11 23:43:46 -0500

    More return statement

---
+++ return_statements/djkhalid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+string say(string phrase){
+    return phrase;
+}
+
+
+
+int main(){
+
+   string newPhrase = say("Lets goooooo");
+
+   cout << newPhrase;
+
+    return 0;
+}
