d5f46a1ce8e940688e32883692d3187338cb1efd
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-13 20:52:17 -0400

    More if

--- if_statements/if.cpp
+++ if_statements/if.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 int main(){
 
+    // bool isMale = false;
     bool isMale = true;
 
-    if( isMale ){
+    bool isTall = true;
+
+
+
+    if( isMale && isTall){
 
         cout << "You are a male";
 
+    } else {
+        cout << "You are not male";
     }
 
     return 0;
---
+++ if_statements/other.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    bool isDark = true;
+
+    bool isSunday = true;
+    if ( isDark && isSunday ){
+        cout << "It is dark and sunday out";
+        
+    }
+
+    return 0;
+}
