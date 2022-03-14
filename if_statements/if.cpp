93d58c07e0d080d076e7036efec3029034d867ed
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-13 20:57:02 -0400

    Negation operator

--- if_statements/if.cpp
+++ if_statements/if.cpp
@@ -4,20 +4,36 @@ using namespace std;
 
 int main(){
 
-    // bool isMale = false;
-    bool isMale = true;
+    bool isMale = false;
+    // bool isMale = true;
 
-    bool isTall = true;
+    // bool isTall = true;
+    bool isTall = false;
 
+    //And
 
+    // if( isMale && isTall){
 
-    if( isMale && isTall){
+    //     cout << "You are a male";
+
+    // } else {
+    //     cout << "You are not male";
+    // }
+    
+
+    //Or
+      if( isMale && isTall){
 
         cout << "You are a male";
 
-    } else {
+    } else if (isMale && !isTall) {
+
+        cout << "This dude is not tall";
+
+    }else {
         cout << "You are not male";
     }
+
 
     return 0;
 }
