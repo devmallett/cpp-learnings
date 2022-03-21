ea69390827af45f9039ded03a3b6a3e715d6847d
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-20 23:44:03 -0400

    Function

---
+++ functions/fucnt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void thisFunct(string hello){
+    
+    cout << hello;
+
+}
+
+int main(){
+
+    thisFunct("Wazzzz Upppppppppppppp");
+    return 0;
+}
