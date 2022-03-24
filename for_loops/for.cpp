965871226b4b403d0c4f7ce7437bfc8210d4097a
devmallett <34248685+devmallett@users.noreply.github.com>
2022-03-23 20:33:11 -0400

    for loop

---
+++ for_loops/for.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace  std;
+
+int main(){
+    
+    // int index = 1;
+
+    // while(index<= 5){
+    //     cout << index << endl;
+    //     index++;
+    // }
+
+    for(int i =1; i <= 5; i++){
+        cout << i << endl;
+
+    }
+    return 0;
+}
