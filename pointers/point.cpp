dfaad856e7895e5bb9a7e00455044611e74ce149
devmallett <34248685+devmallett@users.noreply.github.com>
2022-04-03 23:43:57 -0400

    Pointer variable

--- pointers/point.cpp
+++ pointers/point.cpp
@@ -13,12 +13,17 @@ cout << "GPA: " << &gpa << endl;
 int main() {
 
     int age = 19;
+    int *pAge = &age; //pointer variable 
     double gpa = 2.7;
+    double *pGpa = &gpa; //stores inside memory address
     string name= "Dev";
+    string *pName = &name; //memory address of name variable 
 
-    cout << "Age: " << &age << endl;
-    cout << "Name: " << &name << endl;
-    cout << "GPA: " << &gpa << endl;
+    cout << pAge;
+
+    // cout << "Age: " << &age << endl;
+    // cout << "Name: " << &name << endl;
+    // cout << "GPA: " << &gpa << endl;
     
     return 0;
 }
