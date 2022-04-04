3a15debe18354dcfd7cdfd94f56c0caadbde44ef
devmallett <34248685+devmallett@users.noreply.github.com>
2022-04-03 23:31:24 -0400

    Comments

---
+++ comments/comm.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Hello World, this is a comment
+    //Prints out more text
+    /* 
+    Multi
+    line
+    comments
+    
+    */
+    cout << "Comments are fun"; //This is more comments 
+
+    return 0;
+}
